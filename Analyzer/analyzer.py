8ee488f58f998629daa3146d4b20bbed54c716f5
latifik2 <latifik2@bk.ru>
2024-06-10 22:54:34 +1000

    get VirusTotal analysis results

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -1,13 +1,18 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
+from time import sleep
 from pprint import pprint
 
 
 class Analyzer():
     def __init__(self):
+        load_dotenv()
         self.api_url = "https://www.virustotal.com/api/v3" #/files"
-        self.api_key = "dd19cca619f4b4c5ade651d73d3a3987224f2e6b5db44c2852af397e1b9bdef9"
+        self.api_key = os.getenv("API_KEY")
         self.session = requests.session()
+        self.analysis_status = "queued"
 
     def upload_file(self, file_path: str):
         files = {
@@ -26,10 +31,24 @@ class Analyzer():
             "x-apikey": self.api_key
         }
         r = self.session.get(self.analysis_link, headers=headers)
-        print(r.text)
+        return json.loads(r.text)
+    
+    def analysis_loop(self):
+        analysis_status = None
+        while analysis_status != "completed":
+            analysis_data = self.get_analysis()
+            analysis_status = analysis_data['data']['attributes']['status']
+            print(analysis_status)
+            if analysis_status == "queued":
+                print("Analysis is processing... Please, wait a minute.")
+                sleep(30)
+            # pprint(analysis_data)
+        return analysis_data
 
 
 if __name__ == "__main__":
     analyzer = Analyzer()
     analyzer.upload_file("../Logger/logs.txt")
-    analyzer.get_analysis()
+    analysis_result = analyzer.analysis_loop()['data']['attributes']['results']
+    for vendor, attributes in analysis_result.items():
+        print(vendor, '\t' + attributes['category'])
